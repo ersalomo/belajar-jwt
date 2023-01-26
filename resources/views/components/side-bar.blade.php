fd36bd06be65381a25a40796401ec9ddf90bcd34
ersalomo <2019104830@student.kalbis.ac.id>
2023-01-26 14:11:55 +0700

    update .gitignore

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -3,7 +3,8 @@
         <div id="sidebar-menu" class="sidebar-menu">
             <ul>
                 <li class="active">
-                    <a class="active" href="index.html"><img src="assets/img/icon/menu-icon-01.svg" alt="img">
+                    <a class="active" href="{{ route('login.home') }}"><img src="assets/img/icon/menu-icon-01.svg"
+                            alt="img">
                         <span>Dashboard</span></a>
                 </li>
                 <li class="submenu">
@@ -11,11 +12,11 @@
                             Transactions
                         </span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="transactions.html"><i class="feather-more-horizontal"></i><span> View
+                        <li><a href=""><i class="feather-more-horizontal"></i><span> View
                                     Transactions</span></a></li>
-                        <li><a href="transactions-search.html"><i class="feather-more-horizontal"></i><span>
+                        <li><a href=""><i class="feather-more-horizontal"></i><span>
                                     Transaction Search</span></a></li>
-                        <li><a href="transactions-single.html"><i class="feather-more-horizontal"></i>
+                        <li><a href=""><i class="feather-more-horizontal"></i>
                                 <span>Single Transaction</span></a></li>
                     </ul>
                 </li>
@@ -25,14 +26,14 @@
                         <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
                         <li><a href="{{ route('home.accounts') }}">Employees List</a></li>
-                        <li><a href="leaves.html">Leaves</a></li>
-                        <li><a href="holidays.html">Holidays</a></li>
-                        <li><a href="attendance.html">Attendance</a></li>
+                        <li><a href="">Leaves</a></li>
+                        <li><a href="">Holidays</a></li>
+                        <li><a href="">Attendance</a></li>
                     </ul>
                 </li>
 
                 <li>
-                    <a href="activities.html"><img src="assets/img/icon/menu-icon-10.svg" alt="img">
+                    <a href=""><img src="assets/img/icon/menu-icon-10.svg" alt="img">
                         <span>Activities</span></a>
                 </li>
                 <li class="submenu">
@@ -40,12 +41,12 @@
                             Reports </span>
                         <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="expense-reports.html"> Expense Report </a></li>
-                        <li><a href="invoice-reports.html"> Invoice Report </a></li>
+                        <li><a href=""> Expense Report </a></li>
+                        <li><a href=""> Invoice Report </a></li>
                     </ul>
                 </li>
                 <li>
-                    <a href="settings.html"><img src="assets/img/icon/menu-icon-12.svg" alt="img">
+                    <a href=""><img src="assets/img/icon/menu-icon-12.svg" alt="img">
                         <span>Settings</span></a>
                 </li>
                 <li class="submenu">
@@ -74,7 +75,7 @@
                 </li>
             </ul>
             <div class="logout-btn">
-                <a href="login.html" class="btn btn-primary"><img src="assets/img/icon/lock-out.svg" class="me-2"
+                <a href="" class="btn btn-primary"><img src="assets/img/icon/lock-out.svg" class="me-2"
                         alt="">Logout</a>
             </div>
         </div>
